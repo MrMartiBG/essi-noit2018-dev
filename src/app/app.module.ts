997697e39536e04c253c9c554ff91ee2503a0be7
MrMartiBG <martirace@gmail.com>
2018-03-01 12:47:10 +0200

    Baby steps Material
    Baby steps in material design. Still haven't connected to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatGridListModule, MatInputModule, MatIconModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+  	MatCheckboxModule,
+  	MatGridListModule,
+  	MatInputModule,
+  	MatIconModule,
+  	BrowserAnimationsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-login/form-login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-login',
+  templateUrl: './form-login.component.html',
+  styleUrls: ['./form-login.component.css']
+})
+export class FormLoginComponent {
+}
